aafce0dbbf104bf1afbb2fe904a70c55cf77ba75
Kondwani <kondwagzulu@gmail.com>
2022-07-15 03:58:56 -0700

    low level programming

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strncpy -> for copying purpose
+* @dest: param1
+* @src: param2
+* Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)\
+{
+	strncpy(char *dest, char *src, int n)\
+	return (dest);
+}
